3353c621b5fe56e2ccde419cf915cf1dd3841725
souvik-databricks <souvik-databricks>
2024-10-18 13:07:47 +0000

    Improved error handling

--- job-runner-app/app/app.py
+++ job-runner-app/app/app.py
@@ -1,6 +1,6 @@
 import datetime
 import streamlit as st
-from utils import databricks_instance, trigger_job_run_func
+from utils import databricks_instance, trigger_job_run_func, user_info
 
 st.set_page_config(page_title="Blog-Job-Runner-App", layout="wide")
 
@@ -18,11 +18,14 @@ def process_input():
             current_run_id = job_trigger_response["run_id"]
             st.write(f"""
                         [{datetime.datetime.now()}] 
-                        Run of the job was triggered with this run id: {current_run_id}
+                        Job was triggered with run id: {current_run_id}
                         Report will be sent to: {email}
                     """)
         else:
-            st.error("Either user does not have 'Manage Run' or 'Owner' or 'Admin' permissions on job or job id is invalid.")
+            if job_trigger_response["error_code"] in ["PERMISSION_DENIED","UNAUTHORIZED"]:
+                st.error("You don't have permission to run the job.")
+            else:
+                st.error("Unexpected error occured.")
     else:
         st.error("Invalid job id. Job id can only be numeric.")
 
@@ -32,7 +35,7 @@ st.header("Lakehouse app - Job runner demo")
 with st.form("inputs_form"):
     job_id = st.text_input("Enter Job id", key="job_id")
     user_input = st.text_input("Enter name (optional)",key="name_input")
-    email = st.text_input("Enter email (optional)",key="email_input")
+    email = st.text_input("Enter email (optional)",value=user_info['user_email'] ,key="email_input")
     submit_button = st.form_submit_button("Trigger Job")
 
 # Call function when button is clicked
