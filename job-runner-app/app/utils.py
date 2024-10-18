1f56490af256461866d0f0359fdcf5f34014e5dc
souvik-databricks <souvik-databricks>
2024-10-18 11:35:00 +0000

    Error handling and use of streamlit form

--- job-runner-app/app/utils.py
+++ job-runner-app/app/utils.py
@@ -1,3 +1,5 @@
+import requests
+import json
 import streamlit as st
 from databricks.sdk.core import Config
 
@@ -18,3 +20,17 @@ def _get_user_info():
 user_info = _get_user_info()
 
 databricks_instance = cfg.host
+
+
+def trigger_job_run_func(input_text, jobid, useremail):
+    url = f"{databricks_instance}/api/2.1/jobs/run-now"
+    headers = _get_user_credentials()
+    payload = {
+        "job_id": jobid,
+        "notebook_params": {
+            "email": useremail,
+            "text": input_text
+        }
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    return response.json()
